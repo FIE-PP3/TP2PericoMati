67b3bb76082f3e41701050f0bc42d42ff3469402
lenon65 <lenonpreto65@gmail.com>
2023-10-11 00:41:58 -0300

    actualizacion

--- gato.h
+++ gato.h
@@ -4,28 +4,32 @@
 
 class Gato {
 public:
+    /*Constructor*/
     Gato(float e):energia(e){}
+    /*"Come" un raton, Carga su energia y aumenta en 1 la cantidad de ratones comidos*/
     void ComerRaton(Raton *A)
     {
         energia+=12+A->Getpeso();
         cantRaton++;
     }
-
+    /*Resta el consumo de energia por correr y devuelve dicho consumo*/
     float ConsumoXCorrer(float distancia)
     {
         float consumo=0.5*distancia;
         energia-=consumo;
         return consumo;
     }
-
+    /*Get energia de vuelve la energia*/
     float ConsultaEnergia()
     {
         return energia;
     }
+    /*Devuelve la velocidad teniendo en cuenta la energia en ese momento*/
     float ConsultaVelocidad()
     {
         return 5.0 + energia/10.0;
     }
+    /*De vuelve true o false dependiendo si llega a cazar al raton*/
     bool meConvieneComerRatonA(Raton * A, float mts)
     {
         float consumo=0.5*mts;
@@ -35,13 +39,14 @@ public:
         }
         return false;
     }
+    /*Get Ratones comidos de vuelve un int*/
     int GetRatoncesComidos()
     {
         return cantRaton;
     }
 private:
     float energia;
-    int cantRaton=0;
+    int cantRaton=0; // inicializado en 0
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main (){
     srand(time(nullptr));
+    /*instancia de menu y ejecucion del mismo*/
     Menu menu;
     menu.menu();
     return 0;
