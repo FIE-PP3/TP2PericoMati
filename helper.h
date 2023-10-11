6277b42774b3046e39da0a5cfbe6efc79b5e69b3
lenon65 <lenonpreto65@gmail.com>
2023-10-11 11:53:58 -0300

    actualizacion

--- helper.h
+++ helper.h
@@ -1,12 +1,12 @@
 #ifndef HELPER_H
 #define HELPER_H
-
+/*Librerias*/
 #include<iostream>
 #include<list>
 #include<time.h>
 
 using namespace std;
-
+/*Clases en cada .h*/
 #include"raton.h"
 #include"gato.h"
 #include"menu.h"
